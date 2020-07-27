9d6006cc762c3a31d7f3b6458ca24c2601be5abe
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-07-27 12:25:58 -0500

    Updated urls, views, and templates

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path("", views.index),
+    path("purchase",views.purchase),
+    path("checkout", views.checkout),
 ]
