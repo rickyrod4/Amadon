59db4362e91a4d45a3b0ec4c7fc39d12175f8e4d
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-07-27 01:59:27 -0500

    updated models

--- home/models.py
+++ home/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class Item(models.Model):
-    item = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=None, decimal_places=2)
+class Product(models.Model):
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+class Order(models.Model):
+    quantity = models.IntegerField()
+    total_price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
